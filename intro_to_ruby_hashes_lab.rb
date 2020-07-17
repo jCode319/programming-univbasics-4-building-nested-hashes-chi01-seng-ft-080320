3ffd4901a48b4476d7dc29a9c2992772e004fc70
Jarrell Jackson <jdjack3@gmail.com>
2020-07-17 21:24:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,9 @@ def monopoly_with_second_tier
   # complexity.
   monopoly_with_second_tier = {
     :railroads => {:pieces => 4}
-  }
+  },
+  { :rent_in_dollars => {})
+}
 end
 
 def monopoly_with_third_tier
