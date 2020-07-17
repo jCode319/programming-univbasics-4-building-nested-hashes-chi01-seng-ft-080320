7cac24271519573549962e846739a3322de5c5f9
Jarrell Jackson <jdjack3@gmail.com>
2020-07-17 21:37:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,13 +14,10 @@ def monopoly_with_second_tier
     {
       :pieces => 4,
       :rent_in_dollars =>
-      {
-        :names =>
         {
-          
-        }
+          :names => {},
 
-        }
+          }
       }
     }
 end
