b4718b5d2ca85e1a1705e1d7bf2f2e9361d10f52
Jarrell Jackson <jdjack3@gmail.com>
2020-07-17 21:53:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -82,7 +82,10 @@ def monopoly_with_fourth_tier
             {
               :mortgage_value => 400
             },
-          :shortline_railroad => {}
+          :shortline_railroad => 
+            {
+              :mortgage_value => 800
+            }
         }
       }
     }
